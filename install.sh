d0f462054fdafa563045a80620e63eaa007d8c81
mrFrace <Mr.frace@gmail.com>
2024-02-20 02:13:50 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -75,7 +75,7 @@ cp -f /usr/local/nginx/conf/nginx.conf{,.bak}
 
 # Assume nginx.conf provided by user is in the parent directory
 # If not, you need to modify the path accordingly
-cp -f /nginx.conf /usr/local/nginx/conf/nginx.conf
+cp -f ./nginx.conf /usr/local/nginx/conf/nginx.conf
 
 # Enable ModSecurity in the nginx configuration
 sed -i 's/SecRuleEngine DetectionOnly/SecRuleEngine On/' /usr/local/nginx/conf/modsecurity.conf
@@ -95,7 +95,7 @@ echo -e "Include owasp-crs/crs-setup.conf\nInclude owasp-crs/rules/*.conf" >> /u
 # Setup Nginx service
 # Assume nginx.service provided by user is in the parent directory
 # If not, you need to modify the path accordingly
-cp -f /nginx.service /etc/systemd/system/nginx.service
+cp -f ./nginx.service /etc/systemd/system/nginx.service
 ln -sf /usr/local/nginx/conf /etc/nginx
 ln -sf /usr/local/nginx/sbin/nginx /usr/sbin/
 
